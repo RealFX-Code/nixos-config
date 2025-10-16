71981122c4c6811dcbd40ea89c8c311da8902fc0
Leah <leah@realfx.rocks>
2025-10-16 14:26:00 +0200

    Refresh config for 2025

--- configuration.nix
+++ configuration.nix
@@ -1,236 +1,252 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running "nixos-help").
-
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport32Bit = true;
-  hardware.pulseaudio.support32Bit = true;
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
 
-  imports = [
-    ./hardware-configuration.nix
-  ];
-
-  # grub :D
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.default = "2";
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "leah-nixos"; # Define your hostname.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Oslo";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    packages=[ pkgs.terminus_font ];
-    font="${pkgs.terminus_font}/share/consolefonts/ter-i22b.psf.gz";
-    keyMap="no";
+
+  # I'm norwegian, and speak english, but I like dates and other values in Norwrgian
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nb_NO.UTF-8";
+    LC_IDENTIFICATION = "nb_NO.UTF-8";
+    LC_MEASUREMENT = "nb_NO.UTF-8";
+    LC_MONETARY = "nb_NO.UTF-8";
+    LC_NAME = "nb_NO.UTF-8";
+    LC_NUMERIC = "nb_NO.UTF-8";
+    LC_PAPER = "nb_NO.UTF-8";
+    LC_TELEPHONE = "nb_NO.UTF-8";
+    LC_TIME = "nb_NO.UTF-8";
   };
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "no";
+    variant = "";
+  };
 
-  # Define a user account. Don't forget to set a password with "passwd".
+  # Configure console keymap
+  console.keyMap = "no";
+
+  users.defaultUserShell = pkgs.bash;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.leah = {
     isNormalUser = true;
-    shell= pkgs.zsh;
-    extraGroups = [ "wheel" "libvirtd" "kvm" ]; # Enable "sudo" for the user.
+    description = "Leah";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    shell = pkgs.fish;
   };
 
-  fonts.fonts = with pkgs; [
-    (nerdfonts.override { fonts = ["JetBrainsMono"];})
-    font-awesome
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    terminus-nerdfont
-  ];
+  users.users.root = {
+    shell = pkgs.bash;
+  };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  programs.zsh.enable = true; # temp fix for zsh to be available as a default shell
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
   environment.systemPackages = with pkgs; [
-    vim
-    wget
-    neovim
-    starship
-    brave
-    cargo
-    dunst
-    flameshot
-    fontconfig
-    freetype
-    gcc
     gh
+    eza
+    vim
     git
-    github-desktop
-    gnugrep
-    gnumake
-    kitty
-    luarocks
-    libvirt
-    neovim
-    ninja
-    nodejs
-    feh
-    pavucontrol
-    polkit_gnome
-    python3Full
-    python.pkgs.pip
-    qemu
-    terminus-nerdfont
-    tldr
-    trash-cli
-    unzip
-    variety
-    virt-manager
-    xdg-desktop-portal-gtk
-    xfce.thunar
-
-    # my packages
-    google-chrome # yes i use chrome :D
-    hyfetch
-    bat
-    htop
     btop
-    wlroots
-    wofi
-    hyprland
-    hyprpaper
-    sway
-    vulkan-validation-layers
-    exa
-    hollywood
-    jetbrains-mono
-    prismlauncher
-    temurin-jre-bin
-    temurin-jre-bin-8
+    wget
+    p7zip
+    floorp
+    hyfetch
+    vesktop
+    fastfetch
     wdisplays
-    bat
-    tree
-    xwayland
-    wayland
-    bash
-    egl-wayland
-    foot
-    slurp
-    wl-clipboard
-    grim
-    discord
-    firefox # incase chrome breaks itself
-    cider # apple music :D
-    usbutils
-    networkmanagerapplet
-    vscode.fhs
-    yaru-theme
-    glib
-  ];
-  
-  # dconf :D
-  programs.dconf.enable = true;
+    xfce.thunar
+    xfce.thunar-volman
+    xfce.thunar-archive-plugin
 
-  # Experimental support for Hyprland with Waybar
-  programs.waybar = {
-    enable = true;
-    package = pkgs.waybar.overrideAttrs(old: {
-      mesonFlags = (old.mesonFlags or []) ++ [ "-Dexperimental=true" ];
-      patches = (old.patches or []) ++ [
-        (pkgs.fetchpatch {
-          name = "fix waybar hyprctl";
-          url = "https://aur.archlinux.org/cgit/aur.git/plain/hyprctl.patch?h=waybar-hyprland-git";
-          sha256 = "sha256-pY3+9Dhi61Jo2cPnBdmn3NUTSA8bAbtgsk2ooj4y7aQ=";
-        })
-      ];
-    });
-  };  
+    # Themes
+    themechanger
+    adwaita-icon-theme
+    gnome-themes-extra
+    rose-pine-kvantum
+    kdePackages.qtstyleplugin-kvantum
 
-  # Set defualt browser
-  xdg.mime.enable = true;
-  xdg.mime.defaultApplications = {
-    "text/html" = "/home/leah/.local/bin/google-chrome";
-    "x-scheme-handler/http" = "/home/leah/.local/bin/google-chrome";
-    "x-scheme-handler/https" = "/home/leah/.local/bin/google-chrome";
-    "x-scheme-handler/about" = "/home/leah/.local/bin/google-chrome";
-    "x-scheme-handler/unknown" = "/home/leah/.local/bin/google-chrome";
-  };
+    #
+    # Games
+    #
+    prismlauncher
+    jre8 # Java 8
+    zulu # Java 21
 
-  environment.sessionVariables.DEFAULT_BROWSER = "/home/leah/.local/bin/google-chrome";
-
-  # lord forgive me
-  nixpkgs.config.permittedInsecurePackages = [
-    "openssl-1.1.1u"
-    "python-2.7.18.6"
   ];
 
-  # Fix for Electron apps to run under wayland
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-  # Enable SSH Remote Login
-  services.openssh = {
-    enable = true;
-    settings.PasswordAuthentication = true;
+  environment.variables = {
+    GTK_THEME = "Adwaita:dark";
   };
 
-  # List services that you want to enable:
-  virtualisation.libvirtd.enable = true; 
-
-  services.dbus.enable = true;
-  xdg.portal = {
+  # Steam :D
+  programs.steam = {
     enable = true;
-    wlr.enable = true;
-    # gtk portal needed to make gtk apps happy
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    protontricks.enable = true;
   };
-  security.polkit.enable = true;
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
+
+  # Fonts :D
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.iosevka
+      nerd-fonts.iosevka-term
+    ];
+    fontDir.enable = true;
+  };
+
+  qt = {
+    enable = true;
+    style = "kvantum";
+    platformTheme = "gtk2";
+  };
+
+  # Terminal stuff !! :D
+
+  # Autologin
+  systemd.services.sway-autostart = {
+    enable = true;
+    description = "Autologin";
+    after = [ "graphical-session.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.uwsm}/bin/uwsm start ${pkgs.swayfx}/bin/sway";
+      User = "leah";
+      WorkingDirectory = "%h";
+      StandardInput = "tty";
+      TTYPath = "/dev/tty1";
+      TTYReset = true;
+      TTYVHangup = true;
+      TTYVTDisallocate = true;
+    };
+  };
+#  services.getty = {
+#    autologinUser = "leah";
+#    autologinOnce = true;
+#  };
+
+  environment.shellAliases = {
+    ls = "eza --color=always --group-directories-first --icons -alg";
+    # Temporary alias to "hot-reload" nixos from my test machine
+    updatenix = "wget http://192.168.1.14:3000/configuration.nix; sudo mv configuration.nix /etc/nixos/configuration.nix";
+  };
+
+  programs.fish = {
+    enable = true;
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+
+  # Desktop environment
+
+  programs.uwsm = {
+    enable = true;
+    waylandCompositors = {
+      sway = {
+        prettyName = "Sway";
+        comment = "Run Sway with UWSM";
+        binPath = "/run/current-system/sw/bin/sway";
       };
     };
-    extraConfig = ''
-      DefaultTimeoutStopSec=10s
+  };
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures = {
+      base = true;
+      gtk = true;
+    };
+    package = pkgs.swayfx; # SwayFX ftw
+    extraPackages = with pkgs; [
+      mako # Notification Daemon
+      wofi # Application launcher
+    ];
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      export QT_QPA_PLATFORM=wayland-egl
+      export _JAVA_AWT_WM_NONREPARENTING=1
     '';
-  }; 
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-  networking.enableIPv6 = true;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-  system.autoUpgrade.enable = true;  
-  system.autoUpgrade.allowReboot = false; 
-  system.autoUpgrade.channel = "https://channels.nixos.org/nixos-23.05";
+  };
   
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+  programs.waybar.enable = true;
+
+  programs.foot = {
+    enable = true;
+    theme = "rose-pine";
+    settings = {
+      main = {
+        font = "IosevkaTerm Nerd Font Mono:size=16";
+      };
+      scrollback = {
+        # 10 Million
+        lines = 10000000;
+      };
+      cursor = {
+        style = "beam";
+        blink = "yes";
+      };
+      colors = {
+        alpha = 0.8;
+      };
+    };
+  };
+
+  services.gvfs.enable = true;
+
+  # Soteria is a polkit agent
+  security.soteria.enable = true;
+
+  # XDG Desktop Portal crap
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    wlr.enable = true; # Use WLR xdg desktop portal for a wlr session
+  };
+
+  # Pipewire :D
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  #
+  # Background services
+  #
+
+  services.openssh.enable = true;
+
+  #
+  # Done !
+  #
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+
 }
